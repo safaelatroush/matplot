983f5b17f048b093c97c152f50d9fcdfc0e18f63
safaelatroush <safaelatroush98@gmail.com>
2024-08-19 18:58:17 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -29,22 +29,3 @@ with open ("test2.txt","a") as file:
     duration=end-start
     dist=duration*17150
     dist=round(dist,2)
-
-
-##########################################################
-
-    humid,temp=Adafruit_DHT.read_retry(dht,pin)
-    if temp is not None and humid is not None:
-        file.write(f"distance is : {dist}\n")
-        file.write(f"tempreture is : {temp}\n")
-        file.write(f"humidity is : {humid}\n")
-        # print(f"distance is : {dist}\n")
-        # print(f"tempreture is : {temp}\n")
-        # print(f"humidity is : {humid}\n")
-    else:
-        print("can't calculate tempreture and humidity")
-        file.write("can't calculate temp and humid")
-
-    time.sleep(2)
-
-GPIO.cleanup()
